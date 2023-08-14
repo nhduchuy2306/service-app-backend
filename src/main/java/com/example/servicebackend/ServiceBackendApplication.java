5fbdfb1746199d895fce4dd1bc2ca7beff3f972c
nhduchuy2306 <nguyenhuy1323614@gmail.com>
2023-08-14 23:50:46 +0700

    first commit

---
+++ src/main/java/com/example/servicebackend/ServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.servicebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBackendApplication.class, args);
+	}
+
+}
