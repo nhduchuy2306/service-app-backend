8406e86b2d42cec27f80567065c59830214c7ff1
nhduchuy2306 <nguyenhuy1323614@gmail.com>
2023-08-22 19:29:58 +0700

    update dto

--- src/main/java/com/example/servicebackend/model/dto/UserDto.java
+++ src/main/java/com/example/servicebackend/model/dto/UserDto.java
@@ -1,4 +1,17 @@
 package com.example.servicebackend.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class UserDto {
+    private String userId;
+    private String userName;
+    private String email;
+    private String location;
+    private String image;
+    private String phoneNumber;
 }
