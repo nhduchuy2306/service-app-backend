2321e1071997e42bc630eabaa981947f6e2d4c7b
nhduchuy2306 <nguyenhuy1323614@gmail.com>
2023-08-28 14:28:23 +0700

    update combo

--- src/main/java/com/example/servicebackend/model/dto/ComboDto.java
+++ src/main/java/com/example/servicebackend/model/dto/ComboDto.java
@@ -1,4 +1,21 @@
 package com.example.servicebackend.model.dto;
 
+import com.example.servicebackend.model.enumtype.ComboEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class ComboDto {
+    private Long comboId;
+    private String comboName;
+    private String comboDescription;
+    private String comboPrice;
+    private ComboEnum comboType;
+    private Date startDate;
+    private Date endDate;
 }
--- src/main/java/com/example/servicebackend/model/mapper/ComboMapper.java
+++ src/main/java/com/example/servicebackend/model/mapper/ComboMapper.java
@@ -1,5 +1,7 @@
 package com.example.servicebackend.model.mapper;
 
+import com.example.servicebackend.model.dto.ComboDto;
+import com.example.servicebackend.model.entity.Combo;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
@@ -8,5 +10,8 @@ public interface ComboMapper {
 
     ComboMapper INSTANCE = Mappers.getMapper(ComboMapper.class);
 
+    ComboDto toDto(ComboDto comboDto);
+
+    Combo toEntity(Combo combo);
 
 }
